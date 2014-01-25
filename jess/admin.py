cd6ca7b96a0ac7ac30b3b0beebebfd44594a77ee
Atul Varma <varmaa@gmail.com>
2014-01-25 11:11:57 -0500

    added RSVP inline form to User admin.

--- jess/admin.py
+++ jess/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
 import models
 # Register your models here.
 
-admin.site.register(models.RSVP)
+class RSVPInline(admin.StackedInline):
+	model = models.RSVP
+	can_delete = False
+
+class RSVPUserAdmin(UserAdmin):
+	inlines = (RSVPInline,)
+
+admin.site.unregister(User)
+admin.site.register(User, RSVPUserAdmin)
