a1777968797cb2bb6d90294887a38e7b40c3f188
Atul Varma <varmaa@gmail.com>
2014-01-25 18:50:49 -0500

    added some model validation with tests.

--- jess/models.py
+++ jess/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -12,20 +13,26 @@ class RSVP(models.Model):
         unique=True
     )
     is_attending = models.BooleanField(
-        help_text="Whether the person/family is attending the event.",
         default=False
     )
     number_of_guests = models.SmallIntegerField(
-        help_text="The number of guests the person/family is bringing to "
-                  "the event, including themselves.",
+        help_text="How many people are attending? Please include yourself.",
         default=0
     )
     song = models.CharField(
-        help_text="A song the person/family would like to dance to at "
-                  "the event.",
+        help_text="There will be dancing! We'll try our best to include "
+                  "your favorite song.",
         max_length=200,
         blank=True
     )
 
     def __unicode__(self):
         return u'RSVP for %s' % self.user
+
+    def clean(self):
+        if self.is_attending:
+            if self.number_of_guests < 1:
+              raise ValidationError("Accepted invitations can't have "
+                                    "0 guests.")
+        elif self.number_of_guests > 0:
+            raise ValidationError("Declined invitations can't have guests.")
--- jess/tests.py
+++ jess/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+
+from .models import RSVP
 
 # Create your tests here.
+class RSVPTests(TestCase):
+    def setUp(self):
+        user = User(username='john')
+        user.save()
+        rsvp = RSVP(user=user)
+        rsvp.save()
+        self.user = user
+
+    def test_attending_with_no_guests_is_impossible(self):
+        rsvp = self.user.rsvp
+        rsvp.is_attending = True
+        rsvp.number_of_guests = 0
+        self.assertRaisesRegexp(
+            ValidationError,
+            "Accepted .* can't have 0 guests",
+            rsvp.full_clean
+        )
+
+    def test_not_attending_with_guests_is_impossible(self):
+        rsvp = self.user.rsvp
+        rsvp.is_attending = False
+        rsvp.number_of_guests = 1
+        self.assertRaisesRegexp(
+            ValidationError,
+            "Declined .* can't have guests",
+            rsvp.full_clean
+        )
