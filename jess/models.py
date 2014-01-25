934feb77202bedf66f63ccdb173df0fc95e84fd6
Atul Varma <varmaa@gmail.com>
2014-01-25 17:09:48 -0500

    added help text to the RSVP model fields.

--- jess/models.py
+++ jess/models.py
@@ -4,10 +4,28 @@ from django.contrib.auth.models import User
 # Create your models here.
 class RSVP(models.Model):
     user = models.OneToOneField(User)
-    passphrase = models.CharField(max_length=30, unique=True)
-    is_attending = models.BooleanField(default=False)
-    number_of_guests = models.SmallIntegerField(default=0)
-    song = models.CharField(max_length=200, blank=True)
+    passphrase = models.CharField(
+        help_text="The phrase sent to the person/family in the hard-copy "
+                  "invitation, allowing them to sign into the site "
+                  "without needing to additionally remember a username.",
+        max_length=30,
+        unique=True
+    )
+    is_attending = models.BooleanField(
+        help_text="Whether the person/family is attending the event.",
+        default=False
+    )
+    number_of_guests = models.SmallIntegerField(
+        help_text="The number of guests the person/family is bringing to "
+                  "the event, including themselves.",
+        default=0
+    )
+    song = models.CharField(
+        help_text="A song the person/family would like to dance to at "
+                  "the event.",
+        max_length=200,
+        blank=True
+    )
 
     def __unicode__(self):
         return u'RSVP for %s' % self.user
