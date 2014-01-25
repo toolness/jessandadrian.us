431d6fbe6af06d76afbf891b3d59e8764a9ac497
Atul Varma <varmaa@gmail.com>
2014-01-25 09:20:40 -0500

    adding jess app

---
+++ jess/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class RSVP(models.Model):
+    user = models.OneToOneField(User)
+    passphrase = models.CharField(max_length=30, unique=True)
+    is_attending = models.BooleanField()
+    number_of_guests = models.SmallIntegerField()
