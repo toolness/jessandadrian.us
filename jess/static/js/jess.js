acc3f00dc3abb540ad046194732930f511996f42
Atul Varma <varmaa@gmail.com>
2014-02-17 18:01:19 -0500

    better feature detection

--- jess/static/js/jess.js
+++ jess/static/js/jess.js
@@ -69,23 +69,25 @@ $(document).ready(function() {
     $('div', this).toggleClass('beginAnimation');
   });
 
-  $(document.body).on('click', 'a[href^="/"]', function(e) {
-    var fragment = $(this).attr('href').slice(1);
-    if (doesRouteExist(fragment)) {
-      Backbone.history.navigate(fragment, {trigger: true});
-      return false;
-    }
-  });
+  if (window.history && window.history.pushState)
+    $(document.body).on('click', 'a[href^="/"]', function(e) {
+      var fragment = $(this).attr('href').slice(1);
+      if (doesRouteExist(fragment)) {
+        Backbone.history.navigate(fragment, {trigger: true});
+        return false;
+      }
+    });
 
-  $(document.body).on('submit', 'form[action^="/"]', function(e) {
-    var fragment = $(this).attr('action').slice(1);
-    var routeName = ROUTES.form[fragment];
+  if ('FormData' in window && 'JSON' in window)
+    $(document.body).on('submit', 'form[action^="/"]', function(e) {
+      var fragment = $(this).attr('action').slice(1);
+      var routeName = ROUTES.form[fragment];
 
-    if ($(this).attr('method').toUpperCase() != "POST") return;
-    if (!(routeName && FormSubmitHandlers[routeName])) return;
+      if ($(this).attr('method').toUpperCase() != "POST") return;
+      if (!(routeName && FormSubmitHandlers[routeName])) return;
 
-    return FormSubmitHandlers[routeName](this);
-  });
+      return FormSubmitHandlers[routeName](this);
+    });
 
   Backbone.history.start({
     pushState: true,
