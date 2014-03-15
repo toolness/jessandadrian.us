69b00d8f3fa5fd8eeb65f57230922a5011113e98
Atul Varma <varmaa@gmail.com>
2014-03-15 12:34:27 -0400

    added 'importguests' manage.py command

---
+++ jess/management/commands/importguests.py
@@ -0,0 +1,124 @@
+import csv
+import datetime
+import random
+from optparse import make_option
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand, CommandError
+from django.core.exceptions import ValidationError, ObjectDoesNotExist
+from django.db import transaction
+from django.utils.text import slugify
+
+from jess.models import RSVP
+
+class DryRunFinished(Exception):
+    pass
+
+def convert_rows_to_dicts(rows):
+    column_names = None
+    dicts = []
+    for i, row in enumerate(rows):
+        if i == 0:
+            # Column headers.
+            column_names = tuple([slugify(unicode(val)) for val in row])
+        else:
+            # An actual row with information about a guest.
+            info = {'row': i + 1}
+            for colnum, val in enumerate(row):
+                colname = column_names[colnum]
+                if colname:
+                    info[colname] = val.strip()
+            dicts.append(info)
+    return dicts
+
+class ImportGuestListCommand(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--dry-run',
+            dest='dry_run',
+            default=False,
+            help='don\'t commit imported data to database',
+            action='store_true'
+        ),
+    )
+
+    def generate_passphrase(self, word_list, num_words=3):
+        while True:
+            words = [random.choice(word_list) for i in range(num_words)]
+            phrase = slugify(u'-'.join(words))
+            try:
+                rsvp = RSVP.objects.get(passphrase=phrase)
+            except ObjectDoesNotExist:
+                return phrase
+
+    def get_rows(self, *args, **options):
+        raise NotImplementedError()
+
+    def import_rows(self, rows, word_list):
+        for info in convert_rows_to_dicts(rows):
+            first_name = info['first-name']
+            try:
+                user_id = int(info['id'])
+                last_name = info['last-name']
+                num_guests = int(info['actual-number-of-guests'] or 0)
+                is_attending = bool(info['is-attending'])
+                music_selection = info['music-selection']
+                passphrase = info['passphrase']
+                is_admin = bool(info['is-admin'])
+                email = info['email']
+                username = info['username'] or slugify(u'%s %s' % (first_name, last_name))
+                print "%(user_id)3d %(num_guests)2d %(is_attending)5s %(first_name)s %(last_name)s %(music_selection)s" % locals()
+                try:
+                    user = User.objects.get(pk=user_id)
+                except ObjectDoesNotExist:
+                    user = User(
+                        pk=user_id,
+                        username=username,
+                        first_name=first_name,
+                        last_name=last_name,
+                        is_active=True,
+                        is_staff=is_admin,
+                        is_superuser=is_admin,
+                        email=email
+                    )
+                    user.set_unusable_password()
+                    user.save()
+                try:
+                    rsvp = user.rsvp
+                except ObjectDoesNotExist:
+                    rsvp = RSVP(
+                        user=user,
+                        passphrase=passphrase,
+                        is_attending=is_attending,
+                        song=music_selection,
+                        number_of_guests=num_guests,
+                    )
+                if not rsvp.passphrase:
+                    rsvp.passphrase = self.generate_passphrase(word_list)
+                    print 'new passphrase for %s: %s' % (first_name,
+                                                         rsvp.passphrase)
+                user.save()
+                rsvp.save()
+            except Exception:
+                self.stderr.write('Error importing row '
+                                  '%d (%s)' % (info['row'], first_name))
+                raise
+
+    def handle(self, *args, **options):
+        rows, word_list = self.get_rows_and_word_list(*args, **options)
+
+        try:
+            with transaction.atomic():
+                self.import_rows(rows, word_list)
+                if options['dry_run']: raise DryRunFinished()
+        except DryRunFinished:
+            self.stdout.write("Dry run complete.")
+
+class Command(ImportGuestListCommand):
+    help = 'Import guest list from a CSV file.'
+    args = '<guest-list-filename> <word-list-filename>'
+
+    def get_rows_and_word_list(self, *args, **options):
+        if len(args) != 2:
+            raise CommandError('Please specify two CSV filenames.')
+        rows = [row for row in csv.reader(open(args[0], 'rb'))]
+        word_list = [row[0] for row in csv.reader(open(args[1], 'rb'))]
+        return rows, word_list
